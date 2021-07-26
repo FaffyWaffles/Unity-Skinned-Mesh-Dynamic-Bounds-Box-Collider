fe0fbf54bfe056a646aded4dc8f3a14d967fee9a
FaffyWaffles <55567874+FaffyWaffles@users.noreply.github.com>
2021-07-26 02:03:51 -0500

    Add files via upload

---
+++ DynamicBounds.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicBounds : MonoBehaviour
+{
+    public bool debugbool;
+
+    public GameObject skinnedMeshRendererGameObject;
+    private SkinnedMeshRenderer smr;
+    public BoxCollider boxCollider;
+
+    private Transform rootGO;
+
+
+    private void Awake()
+    {
+        smr = skinnedMeshRendererGameObject.GetComponent<SkinnedMeshRenderer>();
+        rootGO = skinnedMeshRendererGameObject.transform.root;
+    }
+
+    private void Update()
+    {
+        skinnedMeshRendererGameObject.transform.rotation = Quaternion.identity;
+        skinnedMeshRendererGameObject.transform.position = rootGO.localPosition;
+        boxCollider.center = smr.bounds.center - rootGO.transform.position;
+        boxCollider.size = smr.bounds.size;
+    }
+
+}
